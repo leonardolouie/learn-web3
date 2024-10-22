b8f0dbaa5630e94aec52ebd73c3e4353740e3f91
leonardo louie ordonez <leonardolouie30@gmail.com>
2024-10-23 00:27:08 +0800

    feat: added new hands on sample

---
+++ sample-events-handled-to-web3js/SampleContract.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.26;
+
+
+contract ExampleEvent {
+    
+    event TokensSent(address indexed _from, address indexed _to, uint _amount);
+    mapping(address => uint) public tokenBalance;
+
+  
+    constructor() {
+      tokenBalance[msg.sender] = 100;
+    }
+
+    function sendToken(uint _amount, address _to) public   { 
+      require(tokenBalance[msg.sender] >= _amount, "You dont have enough balance to proceed");
+      tokenBalance[msg.sender] -= _amount;
+      tokenBalance[_to] += _amount;
+      emit TokensSent(msg.sender, _to, _amount);
+    }
+}
